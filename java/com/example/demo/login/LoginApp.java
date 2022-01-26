c433c016fb1bb10527d80074dd17dfdad9957f06
seunghyun9 <suji1541@gmail.com>
2022-01-26 18:00:29 +0900

    2022-01-25 18:00 성적표 1차

--- java/com/example/demo/login/LoginApp.java
+++ java/com/example/demo/login/LoginApp.java
@@ -14,13 +14,14 @@ package com.example.demo.login;
 import java.util.Scanner;
 
 public class LoginApp {
-    public  static  String LOGIN_TITLE = "로그인 앱";
+
+    public static String LOGIN_TITLE = "로그인 앱";
     String id;
     String pw;
     String name;
     static String PASSWORD = "abc";
 
-    public String login(String id, String pw, String name){
+    public String login(String id, String pw, String name) {
         this.id = id;
         this.pw = pw;
         this.name = name;
@@ -40,13 +41,13 @@ public class LoginApp {
         }
         아래문과 세미클론이 차이가남 아래 내용은 메모리를 덜 잡아먹어 속도가 빠른 대신 참과 아닌경 우에만 사용가능함
          */
-        res = (pw.equals(PASSWORD)) ? String.format(" %s 님의 비밀번호 %S 가 맞습니다. 로그인 성공",this.name, this.pw)
-                : String.format(" %s 의 아이디는 일치하고, 비밀번호 %s가 틀립니다. 로그인 실패 ",this.id, this.pw);
-
+        res = (pw.equals(PASSWORD)) ? String.format(" %s 님의 비밀번호 %S 가 맞습니다. 로그인 성공", this.name, this.pw)
+                : String.format(" %s 의 아이디는 일치하고, 비밀번호 %s가 틀립니다. 로그인 실패 ", this.id, this.pw);
 
 
         return res;
-
     }
-
 }
+
+
+
