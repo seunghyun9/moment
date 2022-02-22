5647d751122a9567d2984cf3ad47941fc46a1f93
seunghyun9 <suji1541@gmail.com>
2022-02-22 17:59:17 +0900

    2022-02-22 18:00 퀴즈 업데이트

--- java/com/example/demo/quiz/service/Feb07ServiceImpl.java
+++ java/com/example/demo/quiz/service/Feb07ServiceImpl.java
@@ -56,6 +56,24 @@ public class Feb07ServiceImpl implements Feb07Service{
 
     @Override
     public void prime(Scanner scanner) {
+        System.out.println("두 수를 입력하시오.");
+        System.out.println("num 1");
+        int num1 = scanner.nextInt();
+        System.out.println("num 2");
+        int num2 = scanner.nextInt(); // 범위가 될 두 수를 입력
+        int count=0;
+        for(int i = num1; i <= num2; i++) { // num1 <= num2 일 동안만 반복
+            for(int j=2; j<=i; j++) {
+                if(i%j ==0) {
+                    count ++; //2부터 나누었을 때 나머지가 0일 경우 count++
+                }
+            }
+            if(count==1){
+                System.out.println(i+" "); // 소수는 약수가 1을 제외하고 본인 만인 수
+                // count ==1인 경우, 1을 제외하고 약수가 자기 자신 하나이므로 출력
+            }
+            count=0; // 각 수마다 약수의 개수를 세기 위해 초기화
+        }
 
     }
 
@@ -63,20 +81,23 @@ public class Feb07ServiceImpl implements Feb07Service{
     public void leap(Scanner scanner) {
         int year = scanner.nextInt();
         System.out.println((year % 4 == 0)&&(year % 100 != 0)||(year % 400==0)?"윤년입니다":"평년입니다");
-        /*if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        /*
+        풀이자 : 김승현
+        아래 식을 삼항연산자을 이용해 위의 식으로 만들었다.
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
             res = year + "년은 윤년";
         } else {
             res = year + "년은 평년";
         }
         System.out.println(res);
+        위의 식은 if else 문을 사용하였다. year값은 입력 받아 if문에 넣어 4로 나눈값이 0 그리고  100으로 나눈값이 0이 아닌것
+        혹은 year 값을 40으로 나눈값이 0 인것은 res 값이 year + "년은 윤년" 으로 들어간다.
+        그밖의 값은 else 문으로 들어가 res 값이 year + "년은 평년" 으로 들어간다.
+        출력은 res값으로 출력된다.
 
-         */
-        /**
-         * 윤년은 해당연도를 4로 나눈값이 0임을 의미한다.
-         * 그중 100으로 나눈 값이 0인것은 윤년에서 제외하나
-         * 400으로 나눈 값이 0인것은 윤년으로 포함된다.
-         * 그 외의 나머지 연도는 평년이다.
-         * (year % 4 == 0)?(year % 100 != 0)?"윤년입니다":"평년입니다"
+         - 윤년은 해당연도를 4로 나눈값이 0임을 의미 그중 100으로 나눈 값이 0인것은 윤년에서 제외
+         400으로 나눈 값이 0인것은 윤년으로 포함된다.그 외의 나머지 연도는 평년이다.
+         - 출처 :https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=heartflow89&logNo=220966558486
          */
     }
 
--- java/com/example/demo/quiz/service/Feb10ServiceImpl.java
+++ java/com/example/demo/quiz/service/Feb10ServiceImpl.java
@@ -29,17 +29,106 @@ public class Feb10ServiceImpl implements Feb10Service {
         // 랜덤 정수 ( 1 ~ 100) 사이의 10 개 정수 정렬
 
     }
-
+    /**
+     * author : 최민서
+     * des : quick sort
+     * pivot 이라는 기준 값을 세운 후, pivot보다 작은 element는 왼쪽, 큰 것은 오른쪽으로 정렬 반복
+     * 반복이 끝나면 나누어 진 각 부분 안에서 다시 정렬 반복(엘리먼트가 하나씩 남을 때 까지)
+     * pivot은 자유롭게 잡을 수 있지만 해당 해설에서는 중간 인덱스 값으로 설정
+     * 참고: https://javaoop.tistory.com/8
+     * https://www.youtube.com/watch?v=KiIpDEFIjDk
+     */
     @Override
-    public void quickSort() {
-// 랜덤 정수 ( 1 ~ 100) 사이의 10 개 정수 정렬
+    public void quickSort(int[] arr, int start, int end) {
+        if(start<=end){
+            int pivot = partition(arr, start, end); // partition 메소드에서 리턴 받은 값을 pivot으로 사용
+
+            quickSort(arr, start, pivot-1); // 나누어진 왼쪽 배열 정렬
+            quickSort(arr,pivot+1,end); // 나누어진 오른쪽 배열 정렬
+
+        }
     }
 
-    @Override
-    public void mergeSort() {
-        // 랜덤 정수 ( 1 ~ 100) 사이의 10 개 정수 정렬
-
+    private int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2]; // pivot 값 중앙 인덱스로 설정
+        int temp; // 엘리먼트들을 바꾸어줄 때 임시 저장할 variable
+        while (start < end) {
+            while (arr[start] < pivot) {
+                start++; // pivot보다 작을 경우 무시하고 다음 인덱스로 넘어감
+            }
+            while (arr[end] >= pivot) {
+                end--; // pivot 보다 클 경우 무시하고 앞 인덱스로 넘어감
+            }
+            if (start <= end) {
+                temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp; // start 값과 end 값 교환하여 pivot 보다 작은 수는 왼쪽
+                // 큰 수는 오른쪽으로 정렬
+            }
+        }
+        return start;
     }
+    private static int[] sorted;
+    @Override
+    public void mergeSort(int[] a) {
+
+        sorted = new int[a.length];
+        mergeSort(a, 0, a.length - 1);
+        sorted = null;
+    }
+
+    private void mergeSort(int[] a, int left, int right) {
+        if(left == right) return; // element가 하나일 경우 return
+
+        int mid = (left + right) / 2; // 배열을 나눌 중간 부분
+
+        mergeSort(a, left, mid); // 나눈 후 왼쪽 부분 다시 merge 정렬
+        mergeSort(a, mid + 1, right); // 나눈 후 오른쪽 부분 merge 정렬
+
+        merge(a, left, mid, right); // 병합
+    }
+    private static void merge(int[] a, int left, int mid, int right) {
+        int l = left; // 왼쪽 부분 시작점
+        int r = mid + 1; // 오른쪽 부분 시작점
+        int idx = left;
+
+
+        while(l <= mid && r <= right) {//  각부분의 시작점이 끝점보다 작거나 같을때까지 반복
+
+            if(a[l] <= a[r]) {
+                sorted[idx] = a[l];
+                idx++;
+                l++; //왼쪽 원소와 오른쪽 원소 비교 후 왼쪽이 더 작을 경우,
+                // 왼쪽 원소를 새 배열에 넣고 idx++
+            }
+            else {
+                sorted[idx] = a[r];
+                idx++;
+                r++; //왼쪽 원소와 오른쪽 원소 비교 후 오른쪽이 더 작을 경우,
+                // 오른쪽 원소를 새 배열에 넣고 idx++
+            }
+        }
+
+        if(l > mid) { // l > mid 일 경우, 왼쪽 배열의 element가 새 배열에 다 채워짐
+            while(r <= right) {
+                sorted[idx] = a[r];
+                idx++;
+                r++;
+            }
+        }
+        else {
+            while(l <= mid) {
+                sorted[idx] = a[l];
+                idx++;
+                l++; // 오른쪽 부분도 위와 같이 새 배열에 채우기
+            }
+        }
+
+        for(int i = left; i <= right; i++) {
+            a[i] = sorted[i];
+        }  // 새로 채운 배열을 원래 배열에 넣기
+    } /** 출력 부분 어떻게 연결해야할지 막힘
+     */
 
     @Override
     public void magicSquare() {
@@ -55,21 +144,31 @@ public class Feb10ServiceImpl implements Feb10Service {
                 else {System.out.println("숫자만 입력해야 합니다.");return;}
             }break;
         }
+        /*
+        풀이자: 김승현
+        참조: https://dyndy.tistory.com/72
+        위의내용 랜덤값 받는걸로 수정 예정
 
-
+         */
 
 
         int array[][] = new int[deg][deg];
         int a = 1;
         int r = 0;
         int c = deg / 2;
+        /*
+        행 deg 열 deg 값을 갖는 이중배열 arry를 만든다.
+        a r c 의 값을 각각 준다.6
+         */
 
         for(a = 1; a<= (deg*deg); a++){
             array[r][c] = a;
             if (a % deg == 0){
                 r++;
             }
-
+            /* for문 안의 a값은 1  부터 시작되며 a의값은 배열안에 들어가는 값이 되기에 최대 deg*deg까지이다.
+             a를 deg로 나눈 나머지 값이 0이면 r값은 증가한다. 0이 아닌 다른값이 나오면  else문으로 이동한다.
+             */
             else {
                 r--;
                 c--;
@@ -78,15 +177,23 @@ public class Feb10ServiceImpl implements Feb10Service {
                 else if (c <0)
                     c = deg - 1;}
         }
-
-
+        /*
+        else문에서 r값과 c값은 감소하며 여기서 만약 r값이 0보다 작다면 r은 deg-1값이 된다. / r값이 0 보다 작지 않고
+        c가 0보다 작을 때  c값은 deg-1 값이된다.
+        */
+        System.out.println(array[deg][deg]);
+/*
         for(int i=0; i<deg; i++){
             for(int j=0; j<deg; j++){
                 System.out.print(array[i][j]+" ");
             }
-            System.out.println();
-        }
+            System.out.println();       }
+
+ */
+
     }
+
+
 
 
 
